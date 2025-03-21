07d2167b70a74c7cefcb486a41455e88d923ed70
AMIRUL AFIQ BIN ZAKIR HAMDI <149250526+mirulfiq1@users.noreply.github.com>
2025-03-21 21:54:20 +0800

    Update travel_recommendation.js

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -37,6 +37,7 @@ function displayResults(results) {
     });
 }
 
+
 const options = { timeZone: 'America/New_York', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
 const newYorkTime = new Date().toLocaleTimeString('en-US', options);
 console.log("Current time in New York:", newYorkTime);
